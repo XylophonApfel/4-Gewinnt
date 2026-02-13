6a4ea6b1ae9517418cb336cda52c1ab80b59e446
ManuelHerrmannRDF <Manuel.Herrmann@rdf.nuernberg.de>
2026-02-13 08:19:07 +0100

    .

--- management.py
+++ management.py
@@ -55,3 +55,17 @@ def check_diagonal(board, player):
             if all(board[r - i][c + i] == player for i in range(4)):
                 return "Gewonnen"
     return "Nicht gewonnen"
+
+    
+#eingabe der Spieler Namen
+def spieler_eingabe():
+    spieler1 = input("Bitte geben Sie den Namen des 1 Spielers ein: ")
+    spieler2 = input("Bitte geben Sie den Namen des 2 Spielers ein: ")
+        
+    if not spieler1.isalpha() or not spieler2.isalpha():
+        print("Fehler: Namen dürfen nur aus Buchstaben bestehen.")
+        return None
+
+    return spieler1, spieler2
+
+print(spieler_eingabe())
