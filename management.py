b04df922414a42cd898a9eed147a672588319dc7
Aron <lanzendoerfer.aron@gmail.com>
2026-02-13 08:27:40 +0100

    Einfügen

--- management.py
+++ management.py
@@ -55,3 +55,43 @@ def check_diagonal(board, player):
             if all(board[r - i][c + i] == player for i in range(4)):
                 return "Gewonnen"
     return "Nicht gewonnen"
+
+    
+#eingabe der Spieler Namen
+def spieler_eingabe():
+    spieler1 = input("Bitte geben Sie den Namen des 1 Spielers ein: ")
+    spieler2 = input("Bitte geben Sie den Namen des 2 Spielers ein: ")
+        
+    if not spieler1.isalpha() or not spieler2.isalpha():
+        print("Fehler: Namen dürfen nur aus Buchstaben bestehen.")
+        return None
+
+    return spieler1, spieler2
+
+print(spieler_eingabe())
+
+
+def move():
+    while True:
+        try:
+            move_Zeile = int(input("Bitte gebe die Zeile ein (1-6):\n "))
+            if move_Zeile >= 1 and move_Zeile <= 6:
+                break
+            else:
+                print("Zahl muss zwischen 1 und 6 liegen.")
+                continue
+        except ValueError:
+            print("Bitte eine gültige Zahl eingeben.")
+
+        move_Spalte= int(input("Bitte die Spalte eingeben (1-7): \n"))
+    while True:
+        try:
+            move_Spalte= int(input("Bitte die Spalte eingeben (1-7): \n"))
+            if move_Spalte >= 1 and move_Spalte <= 6:
+                break
+            else:
+                print("Zahl muss zwischen 1 und 6 liegen.")
+                continue
+        except ValueError:
+            print("Bitte eine gültige Zahl eingeben.")
+    return move_Spalte, move_Zeile
